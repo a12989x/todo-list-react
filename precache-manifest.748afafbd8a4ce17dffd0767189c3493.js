292784147a3dffa2769b97413790d75f43cccbcc
A12989x <alexis12989x@gmail.com>
2020-07-02 09:51:06 -0500

    Updates

---
+++ precache-manifest.748afafbd8a4ce17dffd0767189c3493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "508c16ea9f43c2deec766365b6f18afb",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "0c6f12a0509ccdbf7cdd",
+    "url": "/todo-list/static/css/main.91287d51.chunk.css"
+  },
+  {
+    "revision": "c5b06b5f82ef41ea0a40",
+    "url": "/todo-list/static/js/2.747103cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.747103cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c6f12a0509ccdbf7cdd",
+    "url": "/todo-list/static/js/main.6017c4b7.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
